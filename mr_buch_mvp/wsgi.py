4e7365ea0b71c5339386552d1468f30378a0a89c
Pralave <pralave.94@outlook.com>
2016-03-16 23:22:12 +0530

    whitenoise installed

--- mr_buch_mvp/wsgi.py
+++ mr_buch_mvp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mr_buch_mvp project.
+WSGI config for mr_buch_alpha project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mr_buch_alpha.settings")
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mr_buch_mvp.settings")
-
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
+
+try:
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
+
+except:
+    pass
